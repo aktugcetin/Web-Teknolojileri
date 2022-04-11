f561b57c6852b1515d8adff9cae80816557cc1ec
Aktuğ Çetin <sqkly@hotmail.com>
2022-04-11 10:52:56 +0300

    Ödevler için yeni klasör oluşturuldu

---
+++ ders-3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$rastgele_sayi = rand(10,100);
+if ($rastgele_sayi >=50) 
+{
+echo "Geçtiniz - Notunuz: $rastgele_sayi";
+}
+
+else
+{
+echo "Kaldınız - Notunuz:$ogr_notu";
+}
+
+
+
+
+
+?>
+
+</body>
+</html>
